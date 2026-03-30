b6d2f460e8c2ae2af12e356b64184b13591a6c80
vrengad <154575720+vrengad@users.noreply.github.com>
2026-03-30 22:07:43 +0200

    Revert "Refactor into Family Trip Organizer: modular tabs, Firebase migration, UI and tests"

--- tabs/tripInfo.js
+++ tabs/tripInfo.js
@@ -7,14 +7,6 @@ let isEditing = false;
 let rerender = () => {};
 
 function escapeHtml(value = "") {
-  const text = String(value ?? "");
-  return text
-    .replace(/&/g, "&amp;")
-    .replace(/</g, "&lt;")
-    .replace(/>/g, "&gt;")
-    .replace(/"/g, "&quot;");
-}
-
   return value
     .replaceAll("&", "&amp;")
     .replaceAll("<", "&lt;")
